93364e060f0dc14a25c5395e07fb708d36dcab9a
Gonzalo D <60021695+MrMisterGD@users.noreply.github.com>
2023-01-05 14:05:18 -0300

    files added via upload
    I still don't know how to upload files directly from git so that's a thing :c

---
+++ script.js
@@ -0,0 +1,13 @@
+var i=1
+function btn1(){
+    var span = document.querySelector("#span1");
+    span.innerText = i++
+}
+function btn2(){
+    var span = document.querySelector("#span2");
+    span.innerText = i++
+}
+function btn3(){
+    var span = document.querySelector("#span3");
+    span.innerText = i++
+}
