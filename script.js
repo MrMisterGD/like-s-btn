66943c0597de3af087d8c04f60894a0fe81e176d
Gonzalo D <60021695+MrMisterGD@users.noreply.github.com>
2023-01-05 14:11:14 -0300

    script updated for initial mistake

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var i=1
+var o=1
+var u=1
 function btn1(){
     var span = document.querySelector("#span1");
     span.innerText = i++
 }
 function btn2(){
     var span = document.querySelector("#span2");
-    span.innerText = i++
+    span.innerText = o++
 }
 function btn3(){
     var span = document.querySelector("#span3");
-    span.innerText = i++
+    span.innerText = u++
 }
